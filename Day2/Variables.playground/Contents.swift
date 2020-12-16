dbbd26e0c126290c134ddcb923ce62b883d9816e
Eric Chen <ericchen@Erics-MacBook-Pro-16.local>
2020-12-16 12:29:54 -0500

    Added GitHubBasics, Day1 swift files

---
+++ Day2/Variables.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+
+// Variables -- declaration and printing
+var str = "Hello, world!"
+var integer: Int = 38
+
+print(integer)
+print("Hi! This is my first swift code! " + str)
+print("Hi! This is my first swift code! This is me using formating! --> \(str)")
+
